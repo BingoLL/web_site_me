3f8605264e8f519419321b0287c2217f4d000621
deskBingo <1358223058@qq.com>
2018-01-26 22:04:13 +0800

    创建了开发环境

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bingo/1-gittest-projiects/01-website-me/web_site_me/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
